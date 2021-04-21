54c6fd7d8c08f6e301bb2bbfa69c3e1c52e2044f
chrishdk <82228725+chrishdk@users.noreply.github.com>
2021-04-21 13:22:29 -0400

    Registro

---
+++ js/validador.js
@@ -0,0 +1,44 @@
+// objeto.metodo(json)
+
+$("#formulariol").validate(
+  {
+    rules: {
+      txtEmail: {
+        required: true,
+
+        email: true,
+      },
+
+      txtContrasena: {
+        required: true,
+
+        minlength: 5,
+      },
+
+      txtRepetirContrasena: {
+        required: true,
+
+        equalTo: "#id_txtContrasena",
+      },
+    },
+    //--> Fin de reglas
+
+    messages: {
+      txtEmail: {
+        required: "Ingrese email",
+        email: "No cumple formato",
+      },
+      txtContrasena: {
+        required: "Ingrese la contraseña",
+        minlength: "Minimo 5 caracteres",
+      },
+
+      txtRepetirContrasena: {
+        required: "Repita la contraseña",
+
+        equalTo: "Debe ser igual al campo contraseña",
+      },
+    },
+  }
+  //-->Fin de mensajes
+);
