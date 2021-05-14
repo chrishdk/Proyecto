ddfe74422e71a4fb637ea95c75f59f0495896b8f
chrishdk <82228725+chrishdk@users.noreply.github.com>
2021-05-14 11:42:53 -0400

    Update
    separacion del footer y funcion del validador

--- js/validador.js
+++ js/validador.js
@@ -1,35 +1,33 @@
 // objeto.metodo(json)
-jQuery(document).ready(function () {
+
 $("#formulario1").validate({
-  rules: {
+    rules: {
+        "txtEmail": {
+            required: true,
+            email: true
+        },
+        "txtContrasena": {
+            required: true,
+            minlength: 5
+        },
+        "txtRepetirContrasena": {
+            required: true,
+            equalTo: '#id_txtContrasena'
+        }
+    }, // --> Fin de reglas
+    messages: {
+        "txtEmail": {
+            required: 'Ingrese email',
+            email: 'No cumple formato'
+        },
+        "txtContrasena": {
+            required: 'Ingrese la contraseña',
+            minlength: 'Mínimo 5 caracteres'
+        },
+        "txtRepetirContrasena": {
+            required: 'Repita la contraseña',
+            equalTo: 'Debe ser igual al campo contraseña'
+        }
+    } //-->Fin de mensajes
 
-      "id_txtEmail": {
-        required: true,
-        email: true
-      },
-      "txtContrasena": {
-          required: true,
-          minlength: 5
-      },
-      "txtRepetirContrasena": {
-          required: true,
-          equalTo: '#id_txtContrasena'
-      }
-  }, // --> Fin de reglas
-  messages: {
-      "id_txtEmail": {
-        required: 'Ingrese email',
-        email: 'No cumple formato'
-      },
-      "txtContrasena": {
-          required: 'Ingrese la contraseña',
-          minlength: 'Mínimo 5 caracteres'
-      },
-      "txtRepetirContrasena": {
-          required: 'Repita la contraseña',
-          equalTo: 'Debe ser igual al campo contraseña'
-      }
-  } //-->Fin de mensajes
-
-});
 });
